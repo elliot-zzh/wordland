a8ba1138f68338511e61fbe7437f91362f992baf
Elliot Zhang <elliot_zh@proton.me>
2026-01-31 00:54:44 +0000

    feat: apply the same list animated effect as article list to author list

--- src/components/AuthorList.vue
+++ src/components/AuthorList.vue
@@ -2,9 +2,9 @@
   <div class="slide-enter-content">
     <div
       v-for="author in AUTHORS"
-      class="list p-20px mb-20px rounded-10px hover:shadow-lg transition-160"
+      class="list p-20px mb-20px rounded-10px hover:shadow-xl transition-240"
     >
-      <h2 class="text-1.3em m-0 mb-5px">{{ author.cnName }}</h2>
+      <h2 class="author-name text-1.3em m-0 mb-5px">{{ author.cnName }}</h2>
       <div class="flex flex-wrap m-10px">
         <span
           v-for="badge in author.badges"
@@ -26,10 +26,24 @@ import { AUTHORS } from '../config'
 </script>
 
 <style scoped>
+
 .list {
-  border: 2px solid #222;
+  border: 2px solid var(--very-dark-gray);
+}
+.list * {
+  text-decoration: none;
+}
+.list:hover {
+  background-color: var(--very-dark-gray);
+}
+.list:hover * {
+  color: #fff;
+  text-decoration: underline var(--very-dark-gray);
 }
 .list:hover .i-tabler-mailbox {
   transform: translateY(-3px);
+}
+.list:hover .author-name * {
+  color: var(--light-sand-yellow);
 }
 </style>
